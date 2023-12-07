0ed10fef7c7842805864ca90a391c57e288745f4
Jermaine Bhoorasingh <jermaine@jermaine.co>
2023-12-07 17:18:20 -0500

    update verbose name for models

--- edl/models.py
+++ edl/models.py
@@ -14,6 +14,10 @@ class Edl(models.Model):
     edl_type = models.CharField(max_length=20, choices=EDL_TYPE_CHOICES)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = "External Dynamic List"
+        verbose_name_plural = "External Dynamic Lists"
+
     def __str__(self):
         return f'{self.name}'.lower()
 
@@ -23,6 +27,11 @@ class EdlEntry(models.Model):
     entry_value = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
     valid_until = models.DateTimeField(default=None)
+
+    class Meta:
+        verbose_name = "External Dynamic List Entry"
+        verbose_name_plural = "External Dynamic Entries"
+
 
     def __str__(self):
         return f'{self.edl.name} {self.entry_value}'.lower()
