3c540206bdc2e9ded776d216cebd6a4ec065665b
Jermaine Bhoorasingh <jermaine@jermaine.co>
2023-12-07 17:22:33 -0500

    uninstalled unfoldadmin, jazzmin

--- paList/settings.py
+++ paList/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'unfold',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
