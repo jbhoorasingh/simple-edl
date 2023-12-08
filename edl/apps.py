b80c863c572d1851c9e333695113f55df6e22571
Jermaine Bhoorasingh <jermaine@jermaine.co>
2023-12-07 22:10:24 -0500

    Update verbose name for application

--- edl/apps.py
+++ edl/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class EdlConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'edl'
+    verbose_name = 'External Dynamic List'
+
+
