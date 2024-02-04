9430862d8e8fa7adac5100efe330e5b2e6a9b4ec
Jermaine Bhoorasingh <jermaine@jermaine.co>
2024-02-03 20:01:50 -0500

    implementing celery for background task; task to clean up expired entries from edl

--- edl/tasks.py
+++ edl/tasks.py
@@ -1,5 +1,8 @@
 from time import sleep
 from celery import shared_task
+from django.utils.timezone import now
+from datetime import timedelta
+from .models import EdlEntry
 
 
 @shared_task()
@@ -8,4 +11,32 @@ def test_task_sample(a, b):
     return {
         'status': True,
         'ans': ans,
+    }
+
+
+@shared_task
+def remove_expired_edl_entries():
+
+    two_days_ago = now() - timedelta(days=2)
+
+    # Filter entries that have been expired for at least 1 days
+    expired_entries = EdlEntry.objects.filter(valid_until__lt=two_days_ago)
+
+
+    # Count the entries for logging or debugging purposes
+    count = expired_entries.count()
+
+    try:
+        # Delete the expired entries
+        expired_entries.delete()
+    # Delete the expired entries
+    except Exception as e:
+        return {
+            'status': False,
+            'message': f"An error occurred: {e}"
+        }
+
+    return {
+        'status': True,
+        'message': f"{count} entries have been deleted"
     }
--- paList/settings.py
+++ paList/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_spectacular',
+    'django_celery_beat',
     'edl',
 ]
 
